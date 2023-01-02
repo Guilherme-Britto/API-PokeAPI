eb45f0f76caf698e0958273bc963b5b20f06a8d5
Guilherme Britto <guilhermefragab@gmail.com>
2023-01-02 00:25:56 -0300

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,26 @@
-/* Monte sua lógica aqui */
+
+function renderCards(array){
+
+    const ul = document.querySelector(".pokemon_list")
+
+    ul.innerText = ''
+    console.log(array)
+    array.forEach(element => {
+        const card = createCard(element)
+        console.log(card)
+        ul.appendChild(card)
+    });
+}
+
+function createCard({name}){
+    const li = document.createElement('li')
+    const image = document.createElement('img')
+    image.src = './src/assets/1 2.svg'
+
+    const p = document.createElement('p')
+    p.innerText = name
+
+    li.append(image, p)
+
+    return li
+}
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,18 @@
 /* Monte a lógica das requisições aqui */
+async function getPokemon () {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(res => {
+        renderCards(res.results)
+        return res
+    })
+
+    return pokemon
+}
+
+getPokemon()
